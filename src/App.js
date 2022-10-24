2c496034dc43c1594218017a716ad6219eb500d3
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-10-24 01:12:40 -0600

    Create user components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import User from './components/User'
+
+const App=()=>{
+  const [currUser, setCurrUser]=useState(null);
+  return (
+    <div className="App">
+      <User currUser={currUser} setCurrUser={setCurrUser} />
+    </div>
+  );
+}
+export default App;
